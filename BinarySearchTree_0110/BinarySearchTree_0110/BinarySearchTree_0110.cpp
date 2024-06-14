ea8224ed90186077d59345a705e81ab335bdce19
Damar2121 <damar.vivoy51@gmail.com>
2024-06-14 10:49:33 +0700

    jika orangtua nya null

--- BinarySearchTree_0110/BinarySearchTree_0110/BinarySearchTree_0110.cpp
+++ BinarySearchTree_0110/BinarySearchTree_0110/BinarySearchTree_0110.cpp
@@ -26,3 +26,13 @@ public:
 
 	void insert(string element) { // Insert a Node in a Binary Search Tree
 		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memmory for the new node
+		newNode->info = element;  // Assign to the data field of the new node
+		newNode->leftchild = nullptr;  //Make the left child of the new node point to null
+		newNode->rightchild = nullptr; // Make the right child of the new node point to null
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+
+		search(element, parent, currentNode);   // Locate the node which will be the parent of the new node
+
+		if (parent == nullptr) {  // If the parent is NULL (Tree is empty)
